a6438da99f67b01194fb7c12615609c4670297ed
maximelopez <maxime.lpz@outlook.fr>
2024-03-12 14:05:56 +0100

    modification format date

--- routes/trips.js
+++ routes/trips.js
@@ -8,13 +8,13 @@ const moment = require('moment');
 router.post('/', (req, res) => {
     const departure = req.body.departure
     const arrival = req.body.arrival
-    const date = req.body.date // format attendu par le frontend : 'DD/MM/YYYY'
-    
+    const date = req.body.date // format attendu par le frontend : 'YYYY-MM-DD'
+
     // Filtre en fonction de départ/arrivée
     Trip.find({ departure: departure, arrival: arrival })
         .then(trips => {
-            // Filtre avec la date
-            const tripsWithDate = trips.filter(trip => moment(trip.date).format('DD/MM/YYYY') === date)
+            // Filtre en fonction de la date - utcOffset(0) pour ne pas créer de décalage avec les heures
+            const tripsWithDate = trips.filter(trip => moment(trip.date).utcOffset(0).format('YYYY-MM-DD') === date);
 
             if (tripsWithDate[0]) {
                 res.json({ result: true, trips: tripsWithDate });
