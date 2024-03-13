845a620e3755e15b4fedde470a0e22daf7b42144
maximelopez <maxime.lpz@outlook.fr>
2024-03-13 10:45:42 +0100

    route bookings

--- routes/carts.js
+++ routes/carts.js
@@ -9,6 +9,7 @@ router.post('/', (req, res) => {
         arrival : req.body.arrival,
         hour : req.body.hour,
         price : req.body.price,
+        isBook: req.body.isBook
     }
     //Enregistre le trajet dans le panier
     const newCart = new Cart(trip)
@@ -30,6 +31,17 @@ router.get('/', (req, res) => {
 router.delete('/:id', (req, res) => {
     Cart.deleteOne({ _id: req.params.id }).then(() => {
         res.json({result: true, message: 'Trajet supprimé'})
+    })
+});
+
+// Returne seulement les trajets isBook : true
+router.get('/bookings', (req, res) => {
+    Cart.find({ isBook: true }).then(bookings => {
+        if(bookings[0]) {
+            res.json({ result: true, bookings: bookings });
+        } else {
+            res.json({ result: false, message: 'Pas de trajets dans le panier.' });
+        }
     })
 });
 
